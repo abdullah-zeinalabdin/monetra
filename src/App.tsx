4e0b172ccbeff2c855ea5e5c6cc055be9cddf522
abdala162 <abdalazeinalabdin@gmail.com>
2026-04-02 04:09:14 +0400

    feat: Add initial dashboard layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "@mui/material"
 import Navbar from './header/Navbar.tsx'
+import Dashboard from "./dashboard/Dashboard.tsx"
 function App() {
   return (
     <Stack height="100vh" bgcolor={'background.default'}>
       <Navbar />
+      <Dashboard />
     </Stack>
   )
 }
---
+++ src/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "@mui/material";
+import SidePages from "./sidePages/SidePages";
+import StatsContainer from "./stats/StatsContainer";
+import TransactionContainer from "./transactions/TransactionCotainer";
+
+export default function Dashboard() {
+    return (
+        <Stack direction='row' spacing={4} p={4} >
+            <SidePages />
+            <StatsContainer />
+            <TransactionContainer />
+        </Stack>
+    )
+}
